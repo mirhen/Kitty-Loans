a959ce8fbde606c996a4a116bec5798df0a17f14
Miriam Haart <mjammer18@gmail.com>
2019-03-02 13:58:58 -0800

    storyboard setup

---
+++ Kitty Loans/Kitty Loans/Controllers/ManagementController.swift
@@ -0,0 +1,30 @@
+//
+//  ManagementController.swift
+//  Kitty Loans
+//
+//  Created by Miriam Haart on 3/2/19.
+//  Copyright © 2019 Miriam Haart. All rights reserved.
+//
+
+import UIKit
+
+class ManagementController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
