bcb72a2fd6b04128fdba1561105974aa1b658ddc
Jason Zhao <jasonjinzhao99@gmail.com>
2019-03-02 14:46:47 -0800

    voice recognition implementation (simple button to stop and start voice recording and then obtain string, display string on label)

--- Kitty Loans/Kitty Loans/Controllers/ManagementController.swift
+++ Kitty Loans/Kitty Loans/Controllers/ManagementController.swift
@@ -7,15 +7,110 @@
 //
 
 import UIKit
+import Speech
 
-class ManagementController: UIViewController {
+class ManagementController: UIViewController, SFSpeechRecognizerDelegate {
+    
+    @IBOutlet weak var voiceButton: UIButton!
+    @IBOutlet weak var detectedTextLabel: UILabel!
+    
+    let audioEngine = AVAudioEngine()
+    let speechRecognizer: SFSpeechRecognizer? = SFSpeechRecognizer()
+    let request = SFSpeechAudioBufferRecognitionRequest()
+    var recognitionTask: SFSpeechRecognitionTask?
+    var isRecording = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.requestSpeechAuthorization()
     }
     
+    //MARK: IBActions and Cancel
+    
+    @IBAction func voiceButtonPressed(_ sender: Any) {
+        if isRecording == true {
+            audioEngine.stop()
+            recognitionTask?.cancel()
+            isRecording = false
+            voiceButton.backgroundColor = UIColor.gray
+        } else {
+            self.recordAndRecognizeSpeech()
+            isRecording = true
+            voiceButton.backgroundColor = UIColor.red
+        }
+    }
+    
+    func cancelRecording() {
+        audioEngine.stop()
+        let node = audioEngine.inputNode
+        node.removeTap(onBus: 0)
+        recognitionTask?.cancel()
+    }
+    
+    //MARK: - Recognize Speech
+    func recordAndRecognizeSpeech() {
+        let node = audioEngine.inputNode
+        let recordingFormat = node.outputFormat(forBus: 0)
+        node.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { buffer, _ in
+            self.request.append(buffer)
+        }
+        audioEngine.prepare()
+        do {
+            try audioEngine.start()
+        } catch {
+            self.sendAlert(message: "There has been an audio engine error.")
+            return print(error)
+        }
+        guard let myRecognizer = SFSpeechRecognizer() else {
+            self.sendAlert(message: "Speech recognition is not supported for your current locale.")
+            return
+        }
+        if !myRecognizer.isAvailable {
+            self.sendAlert(message: "Speech recognition is not currently available. Check back at a later time.")
+            // Recognizer is not available right now
+            return
+        }
+        recognitionTask = speechRecognizer?.recognitionTask(with: request, resultHandler: { result, error in
+            if let result = result {
+                
+                let bestString = result.bestTranscription.formattedString
+                self.detectedTextLabel.text = bestString
+                
+            } else if let error = error {
+                self.sendAlert(message: "There has been a speech recognition error.")
+                print(error)
+            }
+        })
+    }
+    
+    //MARK: - Check Authorization Status
+    func requestSpeechAuthorization() {
+        SFSpeechRecognizer.requestAuthorization { authStatus in
+            OperationQueue.main.addOperation {
+                switch authStatus {
+                case .authorized:
+                    self.voiceButton.isEnabled = true
+                case .denied:
+                    self.voiceButton.isEnabled = false
+                    self.detectedTextLabel.text = "User denied access to speech recognition"
+                case .restricted:
+                    self.voiceButton.isEnabled = false
+                    self.detectedTextLabel.text = "Speech recognition restricted on this device"
+                case .notDetermined:
+                    self.voiceButton.isEnabled = false
+                    self.detectedTextLabel.text = "Speech recognition not yet authorized"
+                }
+            }
+        }
+    }
+    
+    //MARK: - Alert
+    
+    func sendAlert(message: String) {
+        let alert = UIAlertController(title: "Speech Recognizer Error", message: message, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
 
     /*
     // MARK: - Navigation
