54422edd8842765dc1ac6ef162ac990b545a75f8
Miriam Haart <mjammer18@gmail.com>
2019-03-02 13:50:10 -0800

    first comiit with xcode project

---
+++ Kitty Loans/Kitty Loans/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kitty Loans
+//
+//  Created by Miriam Haart on 3/2/19.
+//  Copyright © 2019 Miriam Haart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
